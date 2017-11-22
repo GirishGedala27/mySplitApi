fbeb7cb9239dd9e8c5c54d01cbc16605a7561edf
[GirishGedala27] <[gedalagirish27@gmail.com]>
2017-11-22 16:39:42 -0500

    updated the spltservice with lambda

--- src/main/java/com/myAPI/mySplitAPI/service/SplitService.java
+++ src/main/java/com/myAPI/mySplitAPI/service/SplitService.java
@@ -23,38 +23,37 @@ public class SplitService {
 	// primary method that performs the split functionality
 	public SplitResponse calculateSplit(SplitRequest splitRequest) {
 		List<Person> updatedGroup = new ArrayList<Person>();
-		if(isValidSplit(splitRequest))
-		{
+		if (isValidSplit(splitRequest)) {
 			log.debug("split will be performed");
-		splitValueforEachPerson = (splitRequest.getSplitValue().getTotalSplitAmount())
-				/ (splitRequest.getSplitValue().getTotalSplitGroupCount());
-		
-		for (Person p : splitRequest.getSplitGroup().getSplitGroupMembers()) {
-			updatedValue = splitValueforEachPerson + p.getIOwe();
+			splitValueforEachPerson = (splitRequest.getSplitValue().getTotalSplitAmount())
+					/ (splitRequest.getSplitValue().getTotalSplitGroupCount());
 
-			p.updateIOwe(updatedValue);
-			updatedGroup.add(p);
-		}
+			splitRequest.getSplitGroup().getSplitGroupMembers().forEach(p -> {
+				updatedValue = splitValueforEachPerson + p.getIOwe();
+				p.updateIOwe(updatedValue);
+				updatedGroup.add(p);
+			});
 
-		SplitGroup splitResponseGroup = SplitGroup.builder().splitGroupMembers(updatedGroup).build();
-		log.info("split performed successfully..");
-		return new SplitResponse(splitResponseGroup);
-		}else{
-			throw new SplitException("500.BADSPLIT","Error while performing split, group size and split value members count do not match");
+			SplitGroup splitResponseGroup = SplitGroup.builder().splitGroupMembers(updatedGroup).build();
+			log.info("split performed successfully..");
+			return new SplitResponse(splitResponseGroup);
+		} else {
+			throw new SplitException("500.BADSPLIT",
+					"Error while performing split, group size and split value members count do not match");
 		}
 	}
 
 	// Method to validate the given input request
 	private boolean isValidSplit(SplitRequest splitRequest) {
-		
-		if(splitRequest.getSplitGroup().getSplitGroupMembers().size() == splitRequest.getSplitValue().getTotalSplitGroupCount())
-		{
+
+		if (splitRequest.getSplitGroup().getSplitGroupMembers().size() == splitRequest.getSplitValue()
+				.getTotalSplitGroupCount()) {
 			log.info("split request is valid and split will be performed");
 			return true;
 		}
-		
+
 		return false;
-		
+
 	}
 
 }
